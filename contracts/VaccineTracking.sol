36d64502abaa543c730efb2e6080a710a77e24ec
Jose Block <blockmann3@gmail.com>
2022-06-06 20:57:36 -0600

    Ya mero

--- contracts/VaccineTracking.sol
+++ contracts/VaccineTracking.sol
@@ -5,8 +5,8 @@ contract VaccineTracking {
 
     struct vaccine_batch{
         int64 batch_num;
-        // string vaccine_type;
-        // string location;
+        string vaccine_type;
+        string location;
         int temperature;
         address user;
         int64 received_at;
@@ -17,8 +17,8 @@ contract VaccineTracking {
 
     function createVaccineBatch (
         int64 batch_num, 
-        // string vaccine_type memory, 
-        // string location memory, 
+        string memory vaccine_type, 
+        string memory location, 
         int temperature, 
         int64 received_at, 
         int64 delivered_at
@@ -26,8 +26,8 @@ contract VaccineTracking {
 
         vaccine_batch storage new_vaccine_batch = vaccine_batch_logs[batch_num];
         new_vaccine_batch.batch_num = batch_num;
-        // new_vaccine_batch.vaccine_type = vaccine_type;
-        // new_vaccine_batch.location = location;
+        new_vaccine_batch.vaccine_type = vaccine_type;
+        new_vaccine_batch.location = location;
         new_vaccine_batch.temperature = temperature;
         new_vaccine_batch.user = msg.sender;
         new_vaccine_batch.received_at = received_at;
@@ -37,8 +37,8 @@ contract VaccineTracking {
 
     function getVaccineBatch(int64 batchNum) public view returns (
         int64,
-        // string,
-        // string,
+        string memory,
+        string memory,
         int,
         address,
         int64,
@@ -47,8 +47,8 @@ contract VaccineTracking {
         vaccine_batch storage batch = vaccine_batch_logs[batchNum];
         return(
             batch.batch_num,
-            // batch.vaccine_type
-            // batch.location
+            batch.vaccine_type,
+            batch.location,
             batch.temperature,
             batch.user,
             batch.received_at,
--- test/vaccine_batches.js
+++ test/vaccine_batches.js
@@ -3,11 +3,12 @@ const VaccineTracking = artifacts.require("VaccineTracking");
 contract("VaccineTracking", async accounts => {
     it("should store a vaccine batch log", async () => {
         const vaccine_batches = await VaccineTracking.deployed();
-        const created = await vaccine_batches.createVaccineBatch(10, 34, 19928, 19993);
+        const created = await vaccine_batches.createVaccineBatch(10, "phizer", "Paiz VH2", 34, 19928, 19993);
         const lastbatch = await vaccine_batches.getVaccineBatch(10)
         if (lastbatch != null){
             assert.it
-            console.log("Vaccine Batch: ",lastbatch);
+            console.log("Vaccine Batch: ",lastbatch); 
+             
         }
     });
 })
