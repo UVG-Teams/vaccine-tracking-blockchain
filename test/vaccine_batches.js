7b373b96e69743722bb25ccb642fb705ce13ebb0
Jose Block <blockmann3@gmail.com>
2022-06-07 22:53:48 -0600

    test

--- test/vaccine_batches.js
+++ test/vaccine_batches.js
@@ -1,25 +1,26 @@
 const VaccineTracking = artifacts.require("VaccineTracking");
 
 contract("VaccineTracking", async accounts => {
-    it("should store a vaccine batch log", async () => {
+    it("should store a vaccine batch log and get the first item from the list", async () => {
         const vaccine_batches = await VaccineTracking.deployed();
-        const created = await vaccine_batches.createVaccineBatch(10, "phizer", "Paiz VH2", 34, 19928, 19993);
-        const batch = await vaccine_batches.getVaccineBatch(1)
-        if (batch != null){
-            assert.it
-            console.log("Vaccine Batch: ",batch); 
-             
+        const created = await vaccine_batches.createVaccineBatch(2929, "pfizer", "Paiz VH2", -1, "2022-12-04T22:44");
+        const batches = await vaccine_batches.getVaccineBatches()
+        if (batches != null){
+            if(batches[batches.length-1].batch_num == 2929) {
+                console.log("Vaccine Batch: ",batches[batches.length-1]);
+                assert.it
+            }
         }
     });
     it("should store a vaccine batch and get the last one", async () => {
         const vaccine_batches = await VaccineTracking.deployed();
-        await vaccine_batches.createVaccineBatch(20, "moderna", "san lucas", 39, 19925, 21003);
-        await vaccine_batches.createVaccineBatch(30, "moderna", "Peten", 39, 19924, 22007);
-
-        const lastbatch = await vaccine_batches.getLastVaccineBatch()
-        if (lastbatch != null){
+        await vaccine_batches.createVaccineBatch(20, "moderna", "san lucas", 10, "2022-12-04T22:44");
+        await vaccine_batches.createVaccineBatch(30, "moderna", "Peten", -20, "2022-12-04T22:44");
+        
+        const batches = await vaccine_batches.getVaccineBatches()
+        if (batches != []){
             assert.it
-            console.log("Vaccine Batch: ",lastbatch);
+            console.log("Vaccine Batch: ",batches);
         }
     });
 })
